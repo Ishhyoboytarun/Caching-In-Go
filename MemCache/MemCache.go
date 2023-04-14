19881e8df7db2a7f008f0b2ea93deed4132e70d2
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2023-04-14 06:45:13 +0530

    Create MemCache.go

---
+++ MemCache/MemCache.go
@@ -0,0 +1,48 @@
+/*
+Memcache is a popular distributed memory caching system used to improve the performance and scalability of web applications. 
+It allows storing and retrieving key-value pairs in memory, thereby reducing the need for accessing slower storage systems 
+like databases. In Go, you can use a Memcache client library to interact with the Memcache server and take advantage of its caching capabilities.
+
+There are several Memcache client libraries available for Go, including:
+
+go-memcached: This is a pure Go implementation of the Memcache protocol, providing a low-level interface for interacting 
+with Memcache servers. It supports multiple Memcache servers, consistent hashing, and several advanced features.
+
+memcache: This is a popular third-party Memcache client library for Go, providing a simple and easy-to-use interface for 
+interacting with Memcache servers. It supports most of the common Memcache operations, including get, set, delete, and increment.
+
+gomemcache: This is another popular Memcache client library for Go, providing a simple and intuitive API for interacting 
+with Memcache servers. It supports connection pooling, automatic retries, and other advanced features.
+*/
+
+
+/*
+To use a Memcache client library in Go, you need to first install it using Go's package manager, go get. 
+For example, to install the memcache library, you can run the following command:
+
+go get github.com/bradfitz/gomemcache/memcache
+*/
+
+package MemCache
+
+import (
+    "github.com/bradfitz/gomemcache/memcache"
+)
+
+func main() {
+    // Connect to a Memcache server
+    mc := memcache.New("localhost:11211")
+
+    // Store a value in Memcache
+    err := mc.Set(&memcache.Item{Key: "mykey", Value: []byte("myvalue")})
+    if err != nil {
+        panic(err)
+    }
+
+    // Retrieve a value from Memcache
+    item, err := mc.Get("mykey")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(string(item.Value))
+}
